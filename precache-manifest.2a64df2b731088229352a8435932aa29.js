3529d973cd524ae66cd1d151564658a66d7e3b69
Dima Diachenko <zarg129@gmail.com>
2020-10-16 18:03:11 +0300

    Updates

---
+++ precache-manifest.2a64df2b731088229352a8435932aa29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d9d36ab0eb58ec6d99f79eb5c232c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55cc30df8cc6703a2b38",
+    "url": "./static/css/main.74d9c891.chunk.css"
+  },
+  {
+    "revision": "4564b92c1957fe79cfcc",
+    "url": "./static/js/2.fe5dc388.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fe5dc388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55cc30df8cc6703a2b38",
+    "url": "./static/js/main.f8610453.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
